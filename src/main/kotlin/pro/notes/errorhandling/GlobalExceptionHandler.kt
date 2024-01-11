60a79f656489f987a2799125df5a3c689d756cf0
Pablo Roger Vico <pablo.rogervico@novatec-gmbh.de>
2024-01-11 11:40:41 +0100

    Solved status code issue from exceptions

--- src/main/kotlin/pro/notes/errorhandling/GlobalExceptionHandler.kt
+++ src/main/kotlin/pro/notes/errorhandling/GlobalExceptionHandler.kt
@@ -1,17 +1,20 @@
 package pro.notes.errorhandling
 
-import jakarta.ws.rs.container.ContainerRequestContext
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.CONFLICT
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
-class GlobalExceptionHandler {
-    @ExceptionHandler
+@RestControllerAdvice
+class GlobalExceptionHandle : ResponseEntityExceptionHandler() {
+    @ExceptionHandler(BusinessExceptions::class)
     fun handleBusinessExceptions(
         ex: BusinessExceptions,
-        requestContext: ContainerRequestContext,
+        requestContext: WebRequest,
     ): ResponseEntity<ExceptionResponseWrapper> =
         when (ex) {
             is UserNotFound -> createErrorResponse(NOT_FOUND, ex.message)
